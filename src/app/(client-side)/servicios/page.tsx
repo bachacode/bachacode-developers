93a05023873563eaf857e0606c91e8a25bdeb76a
bachacode <85910843+bachacode@users.noreply.github.com>
2024-11-04 20:08:44 -0400

    feat: hosting section in services page

--- src/app/(client-side)/servicios/page.tsx
+++ src/app/(client-side)/servicios/page.tsx
@@ -5,9 +5,14 @@ import ocOnTheLaptop from "../../../../public/images/oc-on-the-laptop.svg";
 import ocTakingNote from "../../../../public/images/oc-taking-note.svg";
 import ServiceCardAlt from "@/components/ServiceCardAlt";
 import {
+  faCheckCircle,
   faCode,
+  faHardDrive,
   faHeadphones,
+  faMemory,
   faMessage,
+  faMicrochip,
+  faNetworkWired,
   faPalette,
   faServer,
   faShield,
@@ -26,6 +31,7 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 import AdvantageCard from "@/components/AdvantageCard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
 interface TechCardProps {
   icon: IconDefinition;
@@ -41,6 +47,92 @@ function TechCard({ icon, className, title }: TechCardProps) {
         className={`text-gray-600 w-20 h-20 transition-all duration-300 group-hover:scale-105 group-hover:-translate-y-1 ${className}`}
         title={title}
       ></FontAwesomeIcon>
+    </div>
+  );
+}
+
+interface PriceCardProps {
+  title: string;
+  subtitle: string;
+  price: string;
+  benefits: {
+    text: string;
+    icon: IconDefinition;
+  }[];
+}
+
+function PriceCard({ title, subtitle, price, benefits }: PriceCardProps) {
+  return (
+    <div className="bg-white text-zinc-800 rounded-lg p-6 w-full md:w-1/3 shadow-lg border border-zinc-800">
+      <h3 className="text-xl font-semibold text-center mb-4">{title}</h3>
+      <p className="text-center text-zinc-800 mb-6">{subtitle}</p>
+      <div className="text-center mb-6">
+        <span className="text-4xl font-bold">{price}</span>
+        <span className="text-lg font-medium">/mes</span>
+      </div>
+      <h4 className="text-lg font-semibold pb-2">Especificaciones:</h4>
+      <ul className="mb-6 space-y-2">
+        {benefits.map((benefit) => (
+          <li key={benefit.text} className="flex items-center">
+            <FontAwesomeIcon
+              icon={benefit.icon}
+              className="w-full max-w-4 h-auto mr-2 text-accent"
+            ></FontAwesomeIcon>
+            {benefit.text}
+          </li>
+        ))}
+      </ul>
+      <h4 className="text-lg font-semibold pb-2">Incluye:</h4>
+      <ul className="mb-6 space-y-2 pb-4">
+        <li className="flex items-center">
+          <FontAwesomeIcon
+            icon={faCheckCircle}
+            className="w-full max-w-4 h-auto mr-2 text-accent"
+          ></FontAwesomeIcon>
+          Gestión profesional 24/7
+        </li>
+        <li className="flex items-center">
+          <FontAwesomeIcon
+            icon={faCheckCircle}
+            className="w-full max-w-4 h-auto mr-2 text-accent"
+          ></FontAwesomeIcon>
+          SSL Gratis sin coste añadido
+        </li>
+        <li className="flex items-center">
+          <FontAwesomeIcon
+            icon={faCheckCircle}
+            className="w-full max-w-4 h-auto mr-2 text-accent"
+          ></FontAwesomeIcon>
+          Integración con Cloudflare gratuita
+        </li>
+        <li className="flex items-center">
+          <FontAwesomeIcon
+            icon={faCheckCircle}
+            className="w-full max-w-4 h-auto mr-2 text-accent"
+          ></FontAwesomeIcon>
+          Dominio incluido en el precio
+        </li>
+        <li className="flex items-center">
+          <FontAwesomeIcon
+            icon={faCheckCircle}
+            className="w-full max-w-4 h-auto mr-2 text-accent"
+          ></FontAwesomeIcon>
+          Correos personalizados (con el dominio incluido)
+        </li>
+        <li className="flex items-center">
+          <FontAwesomeIcon
+            icon={faCheckCircle}
+            className="w-full max-w-4 h-auto mr-2 text-accent"
+          ></FontAwesomeIcon>
+          99.9% de uptime del servidor
+        </li>
+      </ul>
+      <Link
+        href="/contacto"
+        className="w-full self-center transition-colors duration-200 bg-orange-primary-500 hover:bg-orange-primary-700 text-white py-3 px-6 rounded-lg font-semibold"
+      >
+        Contáctanos
+      </Link>
     </div>
   );
 }
@@ -281,6 +373,99 @@ export default function Servicios() {
               title="PHP"
               className="group-hover:text-indigo-500"
             ></TechCard>
+          </div>
+        </div>
+      </div>
+
+      {/* Hosting section */}
+      <div className="w-full bg-orange-primary-50 flex justify-center">
+        <div className="container py-8 items-center flex flex-col space-y-3">
+          {/* Title*/}
+          <div className="flex flex-col items-center text-center w-full md:w-1/2 space-y-2">
+            <h2
+              id="hosting"
+              className="font-bold text-3xl md:text-4xl tracking-widest text-zinc-900"
+            >
+              Planes de Hosting
+            </h2>
+            <p className="text-lg text-zinc-800">
+              ¡Hostea tu proyecto con uno de nuestros planes flexibles!
+            </p>
+          </div>
+
+          {/* Hosting plans */}
+          <div className="flex flex-col md:flex-row gap-4 p-6">
+            <PriceCard
+              title="Plan Básico"
+              subtitle="Ideal para proyectos personales y sitios web pequeños."
+              price="$15"
+              benefits={[
+                {
+                  icon: faMemory,
+                  text: "1GB Ram",
+                },
+                {
+                  icon: faMicrochip,
+                  text: "1 Core CPU",
+                },
+                {
+                  icon: faHardDrive,
+                  text: "10GB de almacenamiento NVMe",
+                },
+                {
+                  icon: faNetworkWired,
+                  text: "1TB de ancho de banda",
+                },
+              ]}
+            />
+
+            <PriceCard
+              title="Plan Avanzado"
+              subtitle="Diseñado para negocios en crecimiento y sitios web dinámicos."
+              price="$25"
+              benefits={[
+                {
+                  icon: faMemory,
+                  text: "2GB Ram",
+                },
+                {
+                  icon: faMicrochip,
+                  text: "1 Core CPU",
+                },
+                {
+                  icon: faHardDrive,
+                  text: "25GB de almacenamiento NVMe",
+                },
+                {
+                  icon: faNetworkWired,
+                  text: "2TB de ancho de banda",
+                },
+              ]}
+            />
+
+            <PriceCard
+              title="Plan Profesional"
+              subtitle="La mejor opción para proyectos empresariales de alto rendimiento."
+              price="$51"
+              benefits={[
+                {
+                  icon: faMemory,
+                  text: "4GB Ram",
+                },
+                {
+                  icon: faMicrochip,
+                  text: "2 Core CPU",
+                },
+                {
+                  icon: faHardDrive,
+                  text: "50GB de almacenamiento NVMe",
+                },
+                {
+                  icon: faNetworkWired,
+                  text: "4TB de ancho de banda",
+                },
+              ]}
+            />
           </div>
         </div>
       </div>
