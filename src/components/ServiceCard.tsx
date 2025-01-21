bdd25179c2304aa05e006c7db9ca26fe00b96a4c
bachacode <85910843+bachacode@users.noreply.github.com>
2025-01-21 17:12:53 -0400

    fix: update ServiceCard layout for better responsiveness and structure

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
+
 interface ServiceCardProps {
   icon: IconProp;
   title: string;
@@ -15,17 +16,16 @@ export default function ServiceCard({
   iconColor,
 }: ServiceCardProps) {
   return (
-    <div className="group w-full lg:w-1/2 p-4 transition-transform duration-200 delay-75 hover:-translate-y-3">
-      <div className="border border-accent bg-white transition-shadow duration-200 delay-75 shadow-md group-hover:shadow-xl p-6 rounded-lg">
+    <div className="group w-full lg:w-1/2 p-4 transition-transform duration-200 delay-75 hover:-translate-y-3 flex">
+      <div className="border border-accent bg-white transition-shadow duration-200 delay-75 shadow-md group-hover:shadow-xl p-6 rounded-lg flex flex-col flex-grow">
         <FontAwesomeIcon
           icon={icon}
           className={`w-12 h-12 pb-3 ${iconColor}`}
         />
-
         <h3 className="text-lg text-gray-900 font-medium title-font mb-2">
           {title}
         </h3>
-        <p className="leading-relaxed text-base">{children}</p>
+        <p className="leading-relaxed text-base flex-grow">{children}</p>
       </div>
     </div>
   );
