f18c84575d98dc9da57e72cf2960dfc41666bd59
bachacode <85910843+bachacode@users.noreply.github.com>
2025-01-23 13:59:24 -0400

    fix(counter): refactor layout and improve CountUp component usage

--- src/components/HomePageCounter.tsx
+++ src/components/HomePageCounter.tsx
@@ -4,45 +4,51 @@ import CountUp from "react-countup";
 
 export default function HomePageCounter() {
   return (
-    <div className="text-white bg-accent body-font">
-      <div className="container px-5 py-12 mx-auto">
-        <div className="flex flex-wrap -m-4 text-center justify-center w-full">
-          <div className="p-4 sm:w-1/3 w-1/2">
-            <span className="title-font font-medium sm:text-4xl text-3xl text-white">
-              +
+    <div className="body-font bg-accent text-white">
+      <div className="container mx-auto px-5 py-12">
+        <div className="-m-4 flex w-full flex-wrap justify-center text-center">
+          <div className="w-1/2 p-4 sm:w-1/3">
+            <span className="title-font text-3xl font-medium text-white sm:text-4xl">
               <CountUp
+                suffix="+"
                 start={0}
                 end={15}
                 enableScrollSpy
                 scrollSpyOnce
                 duration={4}
-              />
+              >
+                {({ countUpRef }) => <span ref={countUpRef} />}
+              </CountUp>
             </span>
             <p className="leading-relaxed">Proyectos completados</p>
           </div>
-          <div className="p-4 sm:w-1/3 w-1/2">
-            <span className="title-font font-medium sm:text-4xl text-3xl text-white">
-              +
+          <div className="w-1/2 p-4 sm:w-1/3">
+            <span className="title-font text-3xl font-medium text-white sm:text-4xl">
               <CountUp
+                suffix="+"
                 start={0}
                 end={10}
                 enableScrollSpy
                 scrollSpyOnce
                 duration={3}
-              />
+              >
+                {({ countUpRef }) => <span ref={countUpRef} />}
+              </CountUp>
             </span>
             <p className="leading-relaxed">Clientes satisfechos</p>
           </div>
-          <div className="p-4 sm:w-1/3 w-1/2">
-            <span className="title-font font-medium sm:text-4xl text-3xl text-white">
-              +
+          <div className="w-1/2 p-4 sm:w-1/3">
+            <span className="title-font text-3xl font-medium text-white sm:text-4xl">
               <CountUp
+                suffix="+"
                 start={0}
                 end={4}
                 enableScrollSpy
                 scrollSpyOnce
                 duration={2}
-              />
+              >
+                {({ countUpRef }) => <span ref={countUpRef} />}
+              </CountUp>
             </span>
             <p className="leading-relaxed">Años de experiencia</p>
           </div>
