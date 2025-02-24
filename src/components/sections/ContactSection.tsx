65e5f5b26d6d74b392aa40824423c6b3bff69cea
bachacode <85910843+bachacode@users.noreply.github.com>
2025-02-24 19:54:41 -0400

    feat: made ContactSection component inherit content

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { Link } from "@/i18n/routing";
 
-export default function ContactSection() {
+interface ContactSectionProps {
+  body: string;
+  highlight: string;
+  buttonText: string;
+}
+
+export default function ContactSection({
+  body,
+  highlight,
+  buttonText,
+}: ContactSectionProps) {
   return (
     <div className="flex w-full justify-center bg-teal-blue-accent-950 py-6 md:py-20">
       <div className="flex w-3/4 flex-col items-center space-y-6 text-balance text-2xl md:w-1/3 md:items-start">
         <p className="font-semi-bold text-white">
-          Creemos en hacer posible cualquier proyecto que nuestros clientes
-          tengan en mente, ¿estás interesado en construir tu presencia en línea?
-          {"\n"}
-          <strong className="text-primary">Trabaja con nosotros.</strong>
+          {body}
+          <strong className="text-primary">{highlight}</strong>
         </p>
 
         <div>
@@ -17,7 +25,7 @@ export default function ContactSection() {
             href="/contacto"
             className="z-10 rounded-md border-4 border-white px-8 py-3.5 text-base font-semibold uppercase text-white transition-colors duration-300 hover:bg-primary"
           >
-            Contáctanos
+            {buttonText}
           </Link>
         </div>
       </div>
