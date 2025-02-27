7adb9b32c6b68d6a747a9fca7f3ce056bdf60749
bachacode <85910843+bachacode@users.noreply.github.com>
2025-02-27 16:15:50 -0400

    feat: ContactSection props are now optional

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "@/i18n/routing";
+import { useTranslations } from "next-intl";
 
 interface ContactSectionProps {
-  body: string;
-  highlight: string;
-  buttonText: string;
+  body?: string;
+  highlight?: string;
+  buttonText?: string;
 }
 
 export default function ContactSection({
@@ -12,12 +13,13 @@ export default function ContactSection({
   highlight,
   buttonText,
 }: ContactSectionProps) {
+  const t = useTranslations("useContactLocale");
   return (
     <div className="flex w-full justify-center bg-teal-blue-accent-950 py-6 md:py-20">
       <div className="flex w-3/4 flex-col items-center space-y-6 text-balance text-2xl md:w-1/3 md:items-start">
         <p className="font-semi-bold text-white">
-          {body}
-          <strong className="text-primary">{highlight}</strong>
+          {t("body")}
+          <strong className="text-primary">{t("highlight")}</strong>
         </p>
 
         <div>
@@ -25,7 +27,7 @@ export default function ContactSection({
             href="/contact"
             className="z-10 rounded-md border-4 border-white px-8 py-3.5 text-base font-semibold uppercase text-white transition-colors duration-300 hover:bg-primary"
           >
-            {buttonText}
+            {t("button")}
           </Link>
         </div>
       </div>
