55a877bf70918e06cde820f974563cc371da1602
bachacode <85910843+bachacode@users.noreply.github.com>
2025-02-27 19:22:48 -0400

    feat: localized generic metadata

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,9 +1,30 @@
 import BaseLayout from "@/components/layout/BaseLayout";
 import { routing } from "@/i18n/routing";
-import { setRequestLocale } from "next-intl/server";
+import { getTranslations, setRequestLocale } from "next-intl/server";
 import { notFound } from "next/navigation";
 import MainHeader from "@/components/layout/MainHeader";
 import MainFooter from "@/components/layout/MainFooter";
+import { Metadata } from "next";
+
+export async function generateMetadata(
+  props: Omit<
+    {
+      children: React.ReactNode;
+      params: Promise<{ locale: string }>;
+    },
+    "children"
+  >,
+): Promise<Metadata> {
+  const { locale } = await props.params;
+
+  const t = await getTranslations({ locale, namespace: "metadata" });
+
+  return {
+    title: t("title"),
+    description: t("description"),
+    keywords: t("keywords"),
+  };
+}
 
 export default function LocaleLayout({
   children,
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,20 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 const baseUrl = process.env.NEXT_PUBLIC_FRONTEND_URL ?? "https://bachacode.com";
+
 export const metadata: Metadata = {
-  title: "Bachacode Developers | Desarrollo & Consultoría Web",
+  title: "Bachacode Developers | Web Development & Consulting",
   description:
-    "Bachacode Developers se especializa en desarrollo web a medida, ofreciendo sitios estáticos, aplicaciones web y WordPress, ajustados a tus necesidades.",
-  keywords: "desarrollo, programación, wordpress, php, laravel, soporte",
-  authors: [{ name: "Bachacode", url: "https://bachacode.com" }],
+    "Bachacode Developers specializes in custom web development, offering static sites, web applications and WordPress, tailored to your needs.",
+  keywords: "development, programming, wordpress, php, laravel, support",
+  authors: [{ name: "Cristhian Flores", url: "https://bachacode.com" }],
   metadataBase: new URL(baseUrl),
   alternates: {
     canonical: "/",
+    languages: {
+      en: "/",
+      es: "/es",
+    },
   },
   robots: {
     index: true,
