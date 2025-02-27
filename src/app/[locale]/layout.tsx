07ab6e214e5873aea0a4a1b6d0d658784601820f
bachacode <85910843+bachacode@users.noreply.github.com>
2025-02-27 16:14:38 -0400

    feat: LocaleLayout now imports partials instead of a separate layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,4 +1,9 @@
 import BaseLayout from "@/components/layout/BaseLayout";
+import { routing } from "@/i18n/routing";
+import { setRequestLocale } from "next-intl/server";
+import { notFound } from "next/navigation";
+import MainHeader from "@/components/layout/MainHeader";
+import MainFooter from "@/components/layout/MainFooter";
 
 export default function LocaleLayout({
   children,
@@ -7,5 +12,23 @@ export default function LocaleLayout({
   children: React.ReactNode;
   params: { locale: string };
 }>) {
-  return <BaseLayout locale={locale}>{children}</BaseLayout>;
+  // Ensure that the incoming `locale` is valid
+  if (!routing.locales.includes(locale as any)) {
+    notFound();
+  }
+  // Enable static rendering
+  setRequestLocale(locale);
+
+  return (
+    <BaseLayout locale={locale}>
+      {/* Header */}
+      <MainHeader></MainHeader>
+
+      {/* Main Context */}
+      {children}
+
+      {/* Footer */}
+      <MainFooter></MainFooter>
+    </BaseLayout>
+  );
 }
