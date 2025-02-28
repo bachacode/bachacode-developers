ba730a56aa468c6b3f0f986182395770376ae009
bachacode <85910843+bachacode@users.noreply.github.com>
2025-02-27 20:25:39 -0400

    feat: made all localized pages static and moved async logic into layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,10 +1,20 @@
-import BaseLayout from "@/components/layout/BaseLayout";
 import { routing } from "@/i18n/routing";
 import { getTranslations, setRequestLocale } from "next-intl/server";
 import { notFound } from "next/navigation";
 import MainHeader from "@/components/layout/MainHeader";
 import MainFooter from "@/components/layout/MainFooter";
 import { Metadata } from "next";
+import { NextIntlClientProvider } from "next-intl";
+import ProgressBarProvider from "@/components/layout/ProgressBarProvider";
+import { Analytics } from "@vercel/analytics/next";
+import { getMessages } from "next-intl/server";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({ weight: "500", subsets: ["latin"], display: "swap" });
+
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
 
 export async function generateMetadata(
   props: Omit<
@@ -26,30 +36,44 @@ export async function generateMetadata(
   };
 }
 
-export default function LocaleLayout({
+export default async function LocaleLayout({
   children,
-  params: { locale },
+  params,
 }: Readonly<{
   children: React.ReactNode;
   params: { locale: string };
 }>) {
+  const { locale } = await params;
+
   // Ensure that the incoming `locale` is valid
   if (!routing.locales.includes(locale as any)) {
     notFound();
   }
+
   // Enable static rendering
   setRequestLocale(locale);
 
+  // Providing all messages to the client
+  // side is the easiest way to get started
+  const messages = await getMessages();
+
   return (
-    <BaseLayout locale={locale}>
-      {/* Header */}
-      <MainHeader></MainHeader>
-
-      {/* Main Context */}
-      {children}
-
-      {/* Footer */}
-      <MainFooter></MainFooter>
-    </BaseLayout>
+    <html lang={locale}>
+      <body className={`${roboto.className}`}>
+        <NextIntlClientProvider messages={messages}>
+          <ProgressBarProvider>
+            <div className="flex min-h-screen w-full flex-col items-center">
+              {/* Header */}
+              <MainHeader></MainHeader>
+              {/* Main Context */}
+              {children}
+              {/* Footer */}
+              <MainFooter></MainFooter>
+              <Analytics />
+            </div>
+          </ProgressBarProvider>
+        </NextIntlClientProvider>
+      </body>
+    </html>
   );
 }
