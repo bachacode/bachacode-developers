09c94b74c562682b784c7a5d1a3c37d3a3ee4f0d
bachacode <85910843+bachacode@users.noreply.github.com>
2025-03-03 19:47:57 -0400

    fix: added whitespace space between body and highlight in contact section

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -8,8 +8,7 @@ export default function ContactSection() {
     <div className="flex w-full justify-center bg-teal-blue-accent-950 py-6 md:py-20">
       <div className="flex w-3/4 flex-col items-center space-y-6 text-balance text-2xl md:w-1/3 md:items-start">
         <p className="font-semi-bold text-white">
-          {t("body")}
-          <strong className="text-primary">{t("highlight")}</strong>
+          {t("body")} <strong className="text-primary">{t("highlight")}</strong>
         </p>
 
         <div>
