9226da8f9782c4545ae1eaa565e5552f6d868c0a
bachacode <85910843+bachacode@users.noreply.github.com>
2025-08-12 01:36:50 -0400

    feat: updated api contact route to use Turnstile instead of google ReCAPTCHA

--- src/app/api/contacto/route.tsx
+++ src/app/api/contacto/route.tsx
@@ -1,66 +1,58 @@
 import { NextResponse, NextRequest } from "next/server";
 import nodemailer from "nodemailer";
-import generateContactEmail from "@/utils/emailTemplate"; // Importa la función desde tu archivo de utilidades
+import generateContactEmail from "@/utils/emailTemplate";
+import { validateTurnstileToken } from "next-turnstile";
+import { v4 } from "uuid"
+import { getLocale, getTranslations } from "next-intl/server";
 
-// Handles POST requests to /api/contacto
 export async function POST(request: NextRequest) {
-  const secretKey = process?.env?.RECAPTCHA_SECRET_KEY;
+  const { turnstileToken, name, company, email, subject, message } = await request.json();
 
-  try {
-    const { gReCaptchaToken, name, company, email, subject, message } =
-      await request.json();
-    const formData = `secret=${secretKey}&response=${gReCaptchaToken}`;
+  const locale = await getLocale();
+  const t = await getTranslations({ locale, namespace: "contact.form_section.form" });
 
-    const res = await fetch("https://www.google.com/recaptcha/api/siteverify", {
-      method: "POST",
-      body: formData,
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-    });
-    const captcha = await res.json();
-    console.log(captcha);
-    if (captcha && captcha.success && captcha.score > 0.5) {
-      const transporter = nodemailer.createTransport({
-        host: "smtp.sendgrid.net",
-        port: 587,
-        auth: {
-          user: "apikey",
-          pass: process.env.SEND_GRID_API_KEY,
-        },
-      });
+  const validationResponse = await validateTurnstileToken({
+    token: turnstileToken,
+    secretKey: process.env.TURNSTILE_SECRET_KEY!,
+    idempotencyKey: v4(),
+    sandbox: process.env.NODE_ENV === "development",
+  })
 
-      const html = generateContactEmail({
-        name,
-        company,
-        email,
-        subject,
-        message,
-      });
-
-      await transporter.sendMail({
-        from: "support@bachacode.com", // verified sender email
-        to: "support@bachacode.com", // recipient email
-        replyTo: email,
-        subject: `${subject} - ${company ?? "Sin compañia"} - ${name}`, // Subject line
-        html, // html body
-      });
-
-      return NextResponse.json({
-        success: true,
-        message: "¡El correo ha sido enviado exitosamente!",
-      });
-    } else {
-      return NextResponse.json({
-        success: false,
-        message:
-          "Ha fallado la validación de reCAPTCHA, intentelo nuevamente mas tarde.",
-      });
-    }
-  } catch (err) {
-    return NextResponse.json(
-      { message: "¡Ha ocurrido un error enviado el correo!", error: err },
-      { status: 500 },
-    );
+  if (!validationResponse.success) {
+    return NextResponse.json({
+      success: false,
+      message: t("errors.captchaTokenError"),
+    }, { status: 400 });
   }
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.sendgrid.net",
+    port: 587,
+    auth: {
+      user: "apikey",
+      pass: process.env.SEND_GRID_API_KEY,
+    },
+  });
+
+  const html = generateContactEmail({
+    name,
+    company,
+    email,
+    subject,
+    message,
+  });
+
+  await transporter.sendMail({
+    from: "support@bachacode.com", // verified sender email
+    to: "support@bachacode.com", // recipient email
+    replyTo: email,
+    subject: `${subject} - ${company ?? "Sin compañia"} - ${name}`, // Subject line
+    html, // html body
+  });
+
+  return NextResponse.json({
+    success: true,
+    message: t("success"),
+  });
 }
+
