5fbba526824ef1f045618ffd81a65c76c8aa898a
bachacode <85910843+bachacode@users.noreply.github.com>
2025-08-14 20:17:13 -0400

    feat: moved navigation to separate file from routing

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,5 +1,4 @@
 import { defineRouting } from "next-intl/routing";
-import { createNavigation } from "next-intl/navigation";
 
 export const routing = defineRouting({
   // A list of all locales that are supported
@@ -33,10 +32,3 @@ export const routing = defineRouting({
     },
   },
 });
-
-export type Pathnames = keyof typeof routing.pathnames;
-export type Locale = (typeof routing.locales)[number];
-// Lightweight wrappers around Next.js' navigation APIs
-// that will consider the routing configuration
-export const { Link, redirect, usePathname, useRouter, getPathname } =
-  createNavigation(routing);
