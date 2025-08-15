a2fa23a4dab1781f83c0d0c6b5d5198d1fd699e9
bachacode <85910843+bachacode@users.noreply.github.com>
2025-08-14 20:16:52 -0400

    feat: updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,14 +4,8 @@ import { routing } from "./i18n/routing";
 export default createMiddleware(routing);
 
 export const config = {
-  // Matcher entries are linked with a logical "or", therefore
-  // if one of them matches, the middleware will be invoked.
-  matcher: [
-    // Match all pathnames except for
-    // - … if they start with `/api`, `/_next` or `/_vercel`
-    // - … the ones containing a dot (e.g. `favicon.ico`)
-    "/((?!api|_next|_vercel|.*\\..*).*)",
-    // However, match all pathnames within `/users`, optionally with a locale prefix
-    "/([\\w-]+)?/users/(.+)",
-  ],
+  // Match all pathnames except for
+  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
+  // - … the ones containing a dot (e.g. `favicon.ico`)
+  matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
 };
