5893497f1d313a0d4d9cbc02561aa09a5324251c
bachacode <85910843+bachacode@users.noreply.github.com>
2025-08-14 21:11:22 -0400

    feat: added a new rule to enforce using i18n navigation link component

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,6 +8,26 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.config({
     extends: ["next", "prettier", "next/core-web-vitals", "next/typescript"],
+    rules: {
+      // Consistently import navigation APIs from `@/i18n/navigation`
+      "no-restricted-imports": [
+        "error",
+        {
+          name: "next/link",
+          message: "Please import from `@/i18n/navigation` instead.",
+        },
+        {
+          name: "next/navigation",
+          importNames: [
+            "redirect",
+            "permanentRedirect",
+            "useRouter",
+            "usePathname",
+          ],
+          message: "Please import from `@/i18n/navigation` instead.",
+        },
+      ],
+    },
   }),
 ];
 
