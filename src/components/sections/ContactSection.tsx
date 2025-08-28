066f4cb16936d93b963920231e350d5013630662
bachacode <85910843+bachacode@users.noreply.github.com>
2025-08-28 12:11:22 -0400

    feat: redesigned ContactSection component

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -1,24 +1,25 @@
 import React from "react";
 import { Link } from "@/i18n/navigation";
 import { useTranslations } from "next-intl";
+import { Button } from "../ui/button";
+import UnderlinedText from "../common/UnderlinedText";
 
 export default function ContactSection() {
   const t = useTranslations("contact_section");
   return (
-    <div className="bg-teal-blue-accent-950 flex w-full justify-center py-6 md:py-20">
+    <div className="bg-orange-primary-50 flex w-full justify-center py-6 md:py-20 border-y border-primary">
       <div className="flex w-3/4 flex-col items-center space-y-6 text-2xl text-balance md:w-1/3 md:items-start">
-        <p className="font-semi-bold text-white">
-          {t("body")} <strong className="text-primary">{t("highlight")}</strong>
+        <p className="font-semi-bold">
+          {t.rich("body", { keyword: (chunks) => <UnderlinedText>{chunks}</UnderlinedText> })}
         </p>
 
-        <div>
+        <Button size="lg" className="px-8 py-6 rounded-sm text-lg uppercase" asChild>
           <Link
             href="/contact"
-            className="hover:bg-primary z-10 rounded-md border-4 border-white px-8 py-3.5 text-base font-semibold text-white uppercase transition-colors duration-300"
           >
             {t("button")}
           </Link>
-        </div>
+        </Button>
       </div>
     </div>
   );
