227e6f6dc6ca316cd0833363553864dcf4dac727
bachacode <85910843+bachacode@users.noreply.github.com>
2025-08-28 10:50:53 -0400

    feat: redesigned TestimonialCard component

--- src/components/cards/TestimonialCard.tsx
+++ src/components/cards/TestimonialCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faQuoteRight } from "@fortawesome/free-solid-svg-icons";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 
 interface TestimonialCardProps {
   testimony: string;
@@ -15,22 +16,27 @@ export default function TestimonialCard({
 }: TestimonialCardProps) {
   return (
     <div className="w-full p-4 md:w-1/2">
-      <div className="flex h-full flex-col rounded bg-white p-8 shadow-md">
-        <FontAwesomeIcon
-          fixedWidth
-          className="text-accent pb-1.5 text-3xl"
-          icon={faQuoteRight}
-        />
-        <div className="mb-6 grow">
-          <p className="leading-relaxed">{testimony}</p>
-        </div>
-        <span className="inline-flex items-center">
-          <span className="flex grow flex-col">
-            <span className="title-font font-medium text-gray-900">{name}</span>
-            <span className="text-sm text-gray-500">{profession}</span>
-          </span>
-        </span>
-      </div>
+      <Card className="relative h-full space-y-4 rounded shadow-lg hover:shadow-2xl transition-all duration-500 hover:-translate-y-2">
+        <CardContent className="pt-2">
+          <div className="w-10/12">
+            <p className="leading-relaxed">{testimony}</p>
+          </div>
+          <FontAwesomeIcon
+            fixedWidth
+            className="absolute top-4 right-4 text-5xl text-accent opacity-70 pb-1.5"
+            icon={faQuoteRight}
+          />
+        </CardContent>
+        <CardFooter className="flex flex-col items-start space-y-2">
+          <span className="w-full border-t-3 border-accent opacity-50"></span>
+          <div className="flex flex-col">
+            <span className="title-font font-medium">{name}</span>
+            <span className="text-sm text-muted-foreground">
+              {profession}
+            </span>
+          </div>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
