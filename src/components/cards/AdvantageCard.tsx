ed75bb0de2b501a8695f8fa1ac77fd2c9c4cd406
bachacode <85910843+bachacode@users.noreply.github.com>
2025-09-02 16:11:09 -0400

    fix: fixed icon aligment with header

--- src/components/cards/AdvantageCard.tsx
+++ src/components/cards/AdvantageCard.tsx
@@ -1,9 +1,8 @@
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
-import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import { Card, CardContent, CardHeader } from "../ui/card";
-
+import { CardContent, CardHeader } from "../ui/card";
+import { Card } from "./Card";
 interface AdvantageCardProps {
   icon: IconProp;
   title: string;
@@ -18,14 +17,15 @@ export default function AdvantageCard({
   iconColor,
 }: AdvantageCardProps) {
   return (
-    <Card className="group border-accent relative justify-center gap-3 rounded bg-white shadow-lg transition-all duration-500 hover:-translate-y-2 hover:shadow-2xl">
-      <CardHeader className="flex">
-        <FontAwesomeIcon
-          icon={icon}
-          fixedWidth
-          className={`pr-1.5 text-2xl ${iconColor}`}
-        />
-        <h3 className="title-font text-2xl font-medium">{title}</h3>
+    <Card className="group relative justify-center transition-all hover:-translate-y-2 hover:shadow-2xl">
+      <CardHeader className="flex items-start space-x-1.5 text-2xl">
+        <div>
+          <FontAwesomeIcon
+            icon={icon}
+            className={iconColor}
+          />
+        </div>
+        <h3 className="title-font font-medium">{title}</h3>
       </CardHeader>
       <CardContent>
         <p className="text-muted-foreground text-base leading-relaxed">
